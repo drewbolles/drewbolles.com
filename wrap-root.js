a237df8b9654154a358bde11f551358b15fb2936
Drew Bolles <drewbolles@gmail.com>
2018-11-06 00:11:29 -0500

    Adjsut wrap root

--- wrap-root.js
+++ wrap-root.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
+import theme from './src/utils/theme';
 import { ThemeContext } from './src/utils/context';
 
 export default ({ element }) => (
-  <ThemeContext.Provider>{element}</ThemeContext.Provider>
+  <ThemeContext.Provider value={theme}>{element}</ThemeContext.Provider>
 );
