149d1f25a10aa2d3778cb706e7002310f13378a7
Drew Bolles <drewbolles@gmail.com>
2020-01-09 11:56:31 -0500

    Add next offline

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withPlugins = require("next-compose-plugins");
 const withCSS = require("@zeit/next-css");
+const withOffline = require("next-offline");
 const rehypePrism = require("@mapbox/rehype-prism");
 const rehypeReact = require("rehype-react");
 const withMdxEnhanced = require("next-mdx-enhanced")({
@@ -14,6 +15,13 @@ const withMdxEnhanced = require("next-mdx-enhanced")({
   },
 });
 
-module.exports = withPlugins([withMdxEnhanced, withCSS], {
-  pageExtensions: ["mdx", "jsx", "js"],
-});
+module.exports = withPlugins(
+  [
+    withMdxEnhanced,
+    withCSS,
+    [withOffline, { workboxOpts: { swDest: "../public/service-worker.js" } }],
+  ],
+  {
+    pageExtensions: ["mdx", "jsx", "js"],
+  },
+);
