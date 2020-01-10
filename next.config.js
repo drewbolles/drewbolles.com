77c23288bfc65e72cc873481d09f2d04e8afc553
Drew Bolles <drewbolles@gmail.com>
2020-01-10 11:27:21 -0500

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withCSS = require("@zeit/next-css");
+const withPlugins = require("next-compose-plugins");
 const withOffline = require("next-offline");
 const withManifest = require("next-manifest");
 const rehypePrism = require("@mapbox/rehype-prism");
@@ -15,40 +16,26 @@ const withMdxEnhanced = require("next-mdx-enhanced")({
   },
 });
 
-const config = {
-  manifest: {
-    output: "./public",
-    name: "Drew Bolles",
-    short_name: "Drew Bolles",
-    icons: [
-      {
-        src: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/android-chrome-256x256.png",
-        sizes: "256x256",
-        type: "image/png",
-      },
-    ],
-  },
-  workboxOpts: {
-    swDest: "../public/service-worker.js",
-
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "offlineCache",
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
+const config = { pageExtensions: ["js", "mdx"] };
+const manifest = {
+  output: "./public",
+  name: "Drew Bolles",
+  short_name: "Drew Bolles",
+  icons: [
+    {
+      src: "/android-chrome-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      src: "/android-chrome-256x256.png",
+      sizes: "256x256",
+      type: "image/png",
+    },
+  ],
 };
 
-module.exports = withCSS(withMdxEnhanced(withManifest(withOffline(config))));
+module.exports = withPlugins(
+  [withCSS, withMdxEnhanced, [withManifest, { manifest }], withOffline],
+  config,
+);
