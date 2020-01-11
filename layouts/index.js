f5a8f3084f2b87209caa25c164a5e939cb31498e
Drew Bolles <drewbolles@gmail.com>
2020-01-11 11:59:04 -0500

    Add disqus comments

--- layouts/index.js
+++ layouts/index.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { CommentEmbed, DiscussionEmbed } from "disqus-react";
 
 import Text from "../components/Text";
 import ShareLinks from "../components/ShareLinks";
 import Page from "../components/Page";
 
 import "../prism-theme.css";
+import { useRouter } from "next/router";
 
 function PostLayout({ title, __resourcePath, description }) {
+  const disqusConfig = {
+    url: "https://www.drewbolles.com",
+    identifier: __resourcePath.replace(".md", ""),
+    title: title,
+  };
   return ({ children: content }) => {
     return (
       <Page title={title} description={description}>
@@ -14,6 +21,7 @@ function PostLayout({ title, __resourcePath, description }) {
           <Text as="h1">{title}</Text>
           <ShareLinks slug={__resourcePath.replace(".md", "")} title={title} />
           <div className="post-content">{content}</div>
+          <DiscussionEmbed shortname="drewbolles" config={disqusConfig} />
           <style jsx>{`
             .post-content {
               font-size: 18px;
