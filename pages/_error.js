aa41f3426df7f190e37001ba1db09d2758faeee8
Drew Bolles <drewbolles@gmail.com>
2020-01-11 13:54:27 -0500

    Comment out trailing slash fix

--- pages/_error.js
+++ pages/_error.js
@@ -8,13 +8,16 @@ function Error({ statusCode }) {
   );
 }
 
+/**
+ * I've used the expiremental redirects to fix this issue in next.config
+ */
 Error.getInitialProps = ({ req, res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  // trailing slashes are throwing a 404, this is a quick fix
-  if (statusCode === 404 && req.url.endsWith("/")) {
-    res.writeHead(301, { Location: req.url.substring(0, req.url.length - 1) });
-    res.end();
-  }
+  // const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  // // trailing slashes are throwing a 404, this is a quick fix
+  // if (statusCode === 404 && req.url.endsWith("/")) {
+  //   res.writeHead(301, { Location: req.url.substring(0, req.url.length - 1) });
+  //   res.end();
+  // }
   return { statusCode };
 };
 
